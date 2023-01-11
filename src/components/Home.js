02b1c5b4738ad457916169d1ec3c398bf1d9e051
lizdmukami <lizdmukami@gmail.com>
2023-01-11 17:36:32 +0300

    added home elements on the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const H1 = `${name} is a Web Developer from ${city}`
+  return (
+        <div id="home">
+           <h1 style = {{ color: 'firebrick' }}> {H1}</h1>
+        </div>
+    );
 }
 
 export default Home;
